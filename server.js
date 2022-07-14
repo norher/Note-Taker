2340716466c39feb8d13af180883bb695c851c55
Norman Herrera <norman.herrera@ymail.com>
2022-07-13 22:42:32 -0700

    public got linked to js and css

--- server.js
+++ server.js
@@ -8,8 +8,9 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
-// app.use('/api', api);
+app.use(express.static('./public'));
+
+app.use('/api', api);
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'))
